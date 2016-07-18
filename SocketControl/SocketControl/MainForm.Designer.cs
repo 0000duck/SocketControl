33f1ea865d7be3089287a691e0fb40673dd24794
shumaojie <yanjie.jw@163.com>
2016-07-18 15:19:56 +0800

    update

---
+++ SocketControl/SocketControl/MainForm.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace SocketControl
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.BT_Start = new System.Windows.Forms.Button();
+            this.BT_Stop = new System.Windows.Forms.Button();
+            this.timer_Refresh = new System.Windows.Forms.Timer(this.components);
+            this.LBL = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BT_Start
+            // 
+            this.BT_Start.Location = new System.Drawing.Point(90, 52);
+            this.BT_Start.Name = "BT_Start";
+            this.BT_Start.Size = new System.Drawing.Size(75, 23);
+            this.BT_Start.TabIndex = 0;
+            this.BT_Start.Text = "开始";
+            this.BT_Start.UseVisualStyleBackColor = true;
+            this.BT_Start.Click += new System.EventHandler(this.BT_Start_Click);
+            // 
+            // BT_Stop
+            // 
+            this.BT_Stop.Location = new System.Drawing.Point(90, 115);
+            this.BT_Stop.Name = "BT_Stop";
+            this.BT_Stop.Size = new System.Drawing.Size(75, 23);
+            this.BT_Stop.TabIndex = 1;
+            this.BT_Stop.Text = "停止";
+            this.BT_Stop.UseVisualStyleBackColor = true;
+            this.BT_Stop.Click += new System.EventHandler(this.BT_Stop_Click);
+            // 
+            // timer_Refresh
+            // 
+            this.timer_Refresh.Enabled = true;
+            this.timer_Refresh.Tick += new System.EventHandler(this.timer_Refresh_Tick);
+            // 
+            // LBL
+            // 
+            this.LBL.AutoSize = true;
+            this.LBL.Location = new System.Drawing.Point(3, 198);
+            this.LBL.Name = "LBL";
+            this.LBL.Size = new System.Drawing.Size(34, 13);
+            this.LBL.TabIndex = 2;
+            this.LBL.Text = "消息:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.LBL);
+            this.Controls.Add(this.BT_Stop);
+            this.Controls.Add(this.BT_Start);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BT_Start;
+        private System.Windows.Forms.Button BT_Stop;
+        private System.Windows.Forms.Timer timer_Refresh;
+        private System.Windows.Forms.Label LBL;
+    }
+}
+
---
+++ SocketControl/SocketControl/MainForm.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using RobotKit;
+using System.Threading;
+
+namespace SocketControl
+{
+    public partial class MainForm : Form
+    {
+
+       // private SocketClient sc = new SocketClient("192.168.0.254");
+        private SocketClient sc = null;
+        private Thread tr = null;
+        private string Mess = null;
+        private string[] FileLine = null;
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BT_Start_Click(object sender, EventArgs e)
+        {
+            if (tr!=null&&tr.IsAlive)
+            {
+                tr.Abort();
+            }
+
+            tr = new Thread(new ThreadStart(SocketSend));
+            tr.Start();
+
+        }
+
+        public  bool ReadFile(string FileName)
+        {
+            if (FileName == null || !File.Exists(FileName))
+            {
+                return false;
+            }
+
+            FileStream fs = null;
+            StreamReader sr = null;
+            string StrLine = null;
+
+
+            try
+            {
+
+
+                fs = new FileStream(FileName, FileMode.Open, FileAccess.Read);
+                sr = new StreamReader(fs);  //使用StreamReader类来读取文件 
+                sr.BaseStream.Seek(0, SeekOrigin.Begin);
+                int LineCount = 0;
+                FileLine = new string[10000000];
+                while ((StrLine = sr.ReadLine()) != null)
+                {
+
+                    StrLine = StrLine.Trim().ToUpper();
+                    if (StrLine.IndexOf("MOVEL=")<0&&StrLine.IndexOf("MOVEJ=")<0&&StrLine.IndexOf("MOVEC=")<0)
+                    {
+                        continue;
+                    }
+               
+
+
+                    Mess = "行数" + LineCount.ToString() + " " + StrLine;
+                    FileLine[LineCount] = StrLine;
+                    LineCount++;
+
+                }
+                Array.Resize(ref FileLine, LineCount);
+                return true;
+
+            }
+            catch(Exception ex)
+            {
+
+                Mess = ex.Message;
+                return false;
+
+            }
+
+            finally
+            {
+
+                if (sr != null)
+                {
+                    sr.Close();
+                }
+                if (fs != null)
+                {
+                    fs.Close();
+                }
+
+            }
+
+
+        }
+
+
+
+
+        private void SocketSend()
+        {
+            Mess = "开始读取文本";
+            FileLine = null;
+
+            if (!ReadFile(@"C:\Users\yye\Desktop\Recipe.txt") || FileLine == null)
+            {
+                //MessageBox.Show("数据错误");
+                Mess = "数据错误";
+                return;
+            }
+            Mess = "读取文本完成";
+            int i = 0;
+            sc  = new SocketClient("192.168.0.254", 6000);
+            while (i < FileLine.Length)
+            {
+                Mess = "发送:" + i.ToString() + "    \n" + FileLine[i];
+                System.Text.ASCIIEncoding asciiEncoding = new System.Text.ASCIIEncoding();
+                sc.SendData(FileLine[i] + asciiEncoding.GetString(new byte[]{13}));
+                while (sc.ReciveData().IndexOf("success")<0)
+                {
+                    System.Threading.Thread.Sleep(1);
+                }
+                i++;
+            }
+                
+
+
+
+
+
+        }
+
+        private void BT_Stop_Click(object sender, EventArgs e)
+        {
+            if (tr != null && tr.IsAlive)
+            {
+                tr.Abort();
+            }
+        }
+
+        private void Form_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            BT_Stop_Click(this,null);
+        }
+
+        private void timer_Refresh_Tick(object sender, EventArgs e)
+        {
+            LBL.Text = "消息:" + Mess;
+        }
+
+    }
+}
